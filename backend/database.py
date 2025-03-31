6a19776c2bf3cfb12cf278e14cd7e35433cede65
Asafaar <96799228+Asafaar@users.noreply.github.com>
2025-03-31 16:19:05 +0300

    davt

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,10 @@
-# backend/database.py
-
-import pymysql
-from pymysql.cursors import DictCursor
-from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
-
+from urllib.parse import urlparse
 import os
 import psycopg2
 import psycopg2.extras
-from urllib.parse import urlparse
-from dotenv import load_dotenv
-
-load_dotenv()  # טוען את .env במידה ואתה מפתח מקומית
 
 def get_connection():
-    db_url = os.getenv("DATABASE_URL")
+    db_url = os.getenv("DATABASE_URL")  # למשל: postgresql://user:pass@host:5432/db
     url = urlparse(db_url)
 
     return psycopg2.connect(
@@ -21,6 +12,6 @@ def get_connection():
         user=url.username,
         password=url.password,
         host=url.hostname,
-        port=url.port,
-        cursor_factory=psycopg2.extras.RealDictCursor  # מחזיר תוצאה כמו DictCursor
+        port=url.port or 5432,
+        cursor_factory=psycopg2.extras.RealDictCursor
     )
